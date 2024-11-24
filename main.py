7f183d6ae19e4ba3505ab60f2947e7fee9965e48
Jeremiah Dados <jeremiasdados@ufl.edu>
2024-11-24 16:49:46 -0500

    Adding a basic program to load the random EEG data into a list

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import csv
 
+def csv_to_list(file_name):
+    data = []
+    with open(file_name, 'r') as file:
+        csv_reader = csv.reader(file)
+        for row in csv_reader:
+            data.append((float(row[0]), float(row[1])))
+    return data
+
+eeg_data_list = csv_to_list('eeg_data_random.csv')
+
+# First column: time [s], second: amplitude [V]
+print(eeg_data_list[0])
