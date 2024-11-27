3306f28557dd05a3a5d0c5b09b1c75c96f8719ba
Semyon Baykov <simon.baykov@gmail.com>
2024-11-26 20:27:37 -0500

    Added my work on terminal UI so far (TODO)

--- main.py
+++ main.py
@@ -18,10 +18,48 @@ def csv_to_heap(file_name):
     return heap
 
 if __name__ == "__main__":
-    # load the EEG data into a list and into a heap
+    # Terminal UI variables
+    welcome_message = "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n*                                           *\n*    ---      EEG Analyzer      ---         *\n*                                           *\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+    menu_message = "    1. Load a dataset\n    2. Search for a repeating signal\n    3. Detect peaks\n    4. Save results\n    5. Exit\n"
+    eeg_data_loaded = False
+    eeg_data_filename = ''
     eeg_data_list = csv_to_list('eeg_data_random.csv')
     eeg_data_heap = csv_to_heap('eeg_data_random.csv')
+    
 
-    # ask the user how many peaks need to be found
+    print(welcome_message)
 
-    # print out the peaks and the times when they occured
+    # Terminal UI loop 
+    while True:
+        print(menu_message)
+        menu_selection = input()
+
+        if menu_selection == '1':
+            pass
+            # TODO, for now it just loads eeg_data_random.csv 
+
+        elif menu_selection == '2':
+            pass
+            # TODO, maybe this won't even be in our project
+
+        elif menu_selection == '3':
+            # Use the randomly generated data if user doesn't load their own
+            if not eeg_data_loaded:
+                eeg_data_filename = 'eeg_data_random.csv'
+                eeg_data_list = csv_to_list('eeg_data_random.csv')
+                eeg_data_heap = csv_to_heap('eeg_data_random.csv')
+            
+             # Get and print peak 
+            peak = eeg_data_heap.extract_max()
+            print(f'The following peak is detected in {eeg_data_filename}:\n{peak}\n')
+
+
+        elif menu_selection == '4':
+            pass
+            # TODO, will just save whatever to a txt file probaly
+
+        elif menu_selection == '5' or menu_selection.lower() == 'exit':
+            break
+
+        else:
+            print('Error, please try again.\n')
